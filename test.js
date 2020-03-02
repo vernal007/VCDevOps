5a54dbbd4c02daac8b392c0f0dbe2e56764bbab5
Vernal Cooper <vcooper@apdport.com>
2020-03-02 10:14:38 -0500

    json commit

---
+++ test.js
@@ -0,0 +1,23 @@
+myObj =  {"People":[
+    {
+        "Id": "1",
+        "Firstname": "Vernal",
+        "Lastname": "Cooper",
+        "Email": "vcooper@apdport.com"
+    },
+    {
+        "Id": "2",
+        "Firstname": "John",
+        "Lastname": "Doe",
+        "Email": "jdoe@apdport.com"
+    },
+    {
+        "Id": "3",
+        "Firstname": "John1",
+        "Lastname": "Doe1",
+        "Email": "jdoe1@apdport.com"
+    }
+
+]}
+
+console.log(myObj.People[1].Firstname);
